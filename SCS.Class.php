c8170c774d09d4582df7f2c2d3ebcb9f29a2d116
enzio53 <codemaxgames@gmail.com>
2017-12-29 15:11:03 +0100

    Frist launch (In dev)

---
+++ SCS.Class.php
@@ -0,0 +1,33 @@
+<?php
+class SCS{
+
+    public function __construct($SCS = true, $https = false, $log = false){
+        /*
+         * Currently in dev
+         */
+        if($SCS){
+
+            if($https){
+                $this->HTTPS_REDIRECT();
+            }
+
+            if($log){
+                $this->LOG_REQUEST();
+            }
+
+        }
+    }
+
+    private function HTTPS_REDIRECT(){
+        if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+            header('Location: https://'.$_SERVER['SERVER_NAME']);
+        }
+    }
+
+    private function LOG_REQUEST(){
+        /*
+         * Wait please
+         */
+    }
+
+}
